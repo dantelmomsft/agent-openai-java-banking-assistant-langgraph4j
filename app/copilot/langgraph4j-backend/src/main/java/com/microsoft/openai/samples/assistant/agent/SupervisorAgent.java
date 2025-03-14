0e22615c1de8af7eb31558a15e9a12b917fa6d4e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-03-14 20:05:25 +0100

    feat(supervisor): add chat memory management
    - Changed intent handling to return an empty map for the "User" intent.
    - Refactored service creation to use a configurable thread ID, handling potential null values.

--- app/copilot/langgraph4j-backend/src/main/java/com/microsoft/openai/samples/assistant/agent/SupervisorAgent.java
+++ app/copilot/langgraph4j-backend/src/main/java/com/microsoft/openai/samples/assistant/agent/SupervisorAgent.java
@@ -2,13 +2,15 @@ package com.microsoft.openai.samples.assistant.agent;
 
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.output.structured.Description;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.V;
-import org.bsc.langgraph4j.action.AsyncNodeAction;
-import org.bsc.langgraph4j.action.NodeAction;
+import org.bsc.langgraph4j.RunnableConfig;
+import org.bsc.langgraph4j.action.AsyncNodeActionWithConfig;
+import org.bsc.langgraph4j.action.NodeActionWithConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 import static java.lang.String.format;
-import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
+import static org.bsc.langgraph4j.action.AsyncNodeActionWithConfig.node_async;
 
 
 /**
@@ -25,16 +27,15 @@ import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
  * The class provides functionality to evaluate user messages and determine the appropriate action based on pre-defined
  * intent patterns.
  */
-public class SupervisorAgent implements NodeAction<AgentContext> {
+public class SupervisorAgent implements NodeActionWithConfig<AgentContext> {
     private static final Logger log = LoggerFactory.getLogger(SupervisorAgent.class);
-
-    public static String UserProxy = "User";
 
     public enum Intent {
         BillPayment,
         RepeatTransaction,
         TransactionHistory,
-        AccountInfo;
+        AccountInfo,
+        User;
 
         /**
          * Returns a list of all possible names of the {@code Intent} enum constants.
@@ -48,7 +49,6 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
         }
     }
 
-
     /**
      * Represents a router that handles intents and clarifications.
      *
@@ -58,14 +58,15 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
      *         the list of available intents. Otherwise, return null.</li>
      * </ul>
      */
-    static class Router {
+    static class Result {
         // @Description("Intent to route to next. If no intent are identified route to FINISH.")
-        @Description("Intent to route to next. If no intent are identified route to User.")
+        // @Description("Intent to route to next. If no intent are identified route to User.")
+        @Description("Identified intent. Otherwise assume intent to be 'User'.")
         String intent   ;
 
         //@Description("If you don't understand or if an intent is not identified be polite with the user, ask clarifying question also using the list of the available intents.")
         @Description("""
-        If you don't understand or if an intent is not identified be polite with the user,
+        If you don't understand intent or if an intent is not identified be polite with the user,
         ask clarifying question using the list of the available intents. Otherwise null.
         """)
         String clarification;
@@ -87,17 +88,10 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
          * @return A {@link Map} containing either "intent" or "clarification" based on the current state, if any. If neither is available, throws an {@link IllegalStateException}.
          */
         public Map<String,Object> toMap() {
+            if( clarification == null || clarification.isBlank() ) {
+                return Map.of( "intent", intent );
+            }
             return Map.of( "intent", intent, "clarification", clarification );
-            /*
-            if(UserProxy.equalsIgnoreCase(intent) ) {
-                return Map.of("clarification", clarification );
-            }
-            else if( clarification == null ) {
-                return Map.of("intent", intent );
-            }
-
-            throw new IllegalStateException("SupervisorAgent could not infer either 'intent' or 'clarification'!");
-            */
         }
     }
 
@@ -114,7 +108,7 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
          *
          * @param intents A JSON array containing the available intents (e.g., "CheckBalance", "PayBills").
          * @param userMessage The user's input message as a JSON string containing details about the request.
-         * @return A {@link Router} representing the detected intent based on the user's message and available intents.
+         * @return A {@link Result} representing the detected intent based on the user's message and available intents.
          */
         @SystemMessage( """
         You are a personal financial advisor who help bank customers manage their banking accounts and services.
@@ -126,10 +120,11 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
         
         Don't add any comments in the output or other characters, just use json format.
         """)
-        Router evaluate(@V("intents") String intents, @dev.langchain4j.service.UserMessage  String userMessage);
+        Result evaluate(@V("intents") String intents, @dev.langchain4j.service.UserMessage  String userMessage);
     }
 
-    private final Service service;
+    final ChatLanguageModel model;
+    private Service service;
 
     /**
      * Creates an {@code AsyncNodeAction} with a new {@code SupervisorAgent}.
@@ -137,8 +132,8 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
      * @param model the chat language model to use
      * @return the created {@code AsyncNodeAction}
      */
-    public static AsyncNodeAction<AgentContext> of(ChatLanguageModel model ) {
-        return node_async( new SupervisorAgent(model ));
+    public static AsyncNodeActionWithConfig<AgentContext> of(ChatLanguageModel model ) {
+        return node_async( new SupervisorAgent(model) );
     }
 
     /**
@@ -147,10 +142,22 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
      * @param model the {@link ChatLanguageModel} to be used by this agent.
      */
     private SupervisorAgent( ChatLanguageModel model ) {
-
-        service = AiServices.create( Service.class, model );
+        this.model = model;
     }
 
+    private Service service( AgentContext state, RunnableConfig config ) {
+        if( service==null ) {
+            service = config.threadId()
+                    .map( threadId ->
+                            AiServices.builder( Service.class )
+                                .chatLanguageModel( model )
+                                .chatMemory( MessageWindowChatMemory.withMaxMessages(10) )
+                                .build())
+                    .orElseGet( () -> AiServices.create( Service.class, model));
+
+        }
+        return service;
+    }
     /**
      * Applies the given agent context to determine a route.
      * 
@@ -158,7 +165,12 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
      * @return A map representing the calculated route.
      */
     @Override
-    public Map<String, Object> apply(AgentContext state) {
+    public Map<String, Object> apply(AgentContext state, RunnableConfig config) {
+
+        if( state.intent().isPresent() && state.intent().get().equals(Intent.User.name())) {
+            return Map.of();
+        }
+
         var m = String.join(",", Intent.names());
         var message = state.lastMessage().orElseThrow();
         var text = switch( message.type() ) {
@@ -167,11 +179,11 @@ public class SupervisorAgent implements NodeAction<AgentContext> {
             default -> throw new IllegalStateException("unexpected message type: " + message.type() );
         };
 
-        var route = service.evaluate( m, text );
+        var result = service( state, config ).evaluate( m, text );
 
-        log.debug( "Route {}", route);
+        log.debug( "supervisor result {}", result);
 
-        return route.toMap();
+        return result.toMap();
 
     }
 }
