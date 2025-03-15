7fbc32e4544faffb8a6941aadfb81be506823875
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-03-15 18:08:21 +0100

    refactor(AgentMemory.java): extend GraphChatMemory to manage agent state
    The current implementation of AgentMemory is replaced with an extension of GraphChatMemory, which allows for efficient management of the agent's state in a graph-based memory system.

--- app/copilot/langgraph4j-backend/src/main/java/com/microsoft/openai/samples/assistant/agent/AgentMemory.java
+++ app/copilot/langgraph4j-backend/src/main/java/com/microsoft/openai/samples/assistant/agent/AgentMemory.java
@@ -1,62 +1,13 @@
 package com.microsoft.openai.samples.assistant.agent;
 
-import dev.langchain4j.data.message.ChatMessage;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.memory.ChatMemory;
-import org.bsc.langgraph4j.state.AgentState;
-import org.bsc.langgraph4j.state.RemoveByHash;
+import org.bsc.langgraph4j.langchain4j.memory.GraphChatMemory;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
-public class AgentMemory implements ChatMemory {
+public class AgentMemory extends GraphChatMemory<AgentWorkflowState> {
 
     private AgentWorkflowState state;
 
     public AgentMemory() {
-    }
-
-    void setState( AgentWorkflowState state ) {
-        this.state = state ;
-    }
-
-    @Override
-    public Object id() {
-        return "state";
-    }
-
-    @Override
-    public void add(ChatMessage message) {
-        Objects.requireNonNull(state);
-        if( message instanceof SystemMessage systemMessage ) {
-            var prevSystemMessage = state.memory().stream()
-                    .filter(m -> m instanceof SystemMessage)
-                    .map(SystemMessage.class::cast)
-                    .findAny()
-                    ;
-            if( prevSystemMessage.isPresent() &&  !prevSystemMessage.get().equals(systemMessage) ) {
-
-                // Replace System Message in memory
-                AgentState.updateState(state,
-                        Map.of("memory", List.of(RemoveByHash.of(prevSystemMessage.get()), message)),
-                        AgentWorkflowState.SCHEMA);
-                return;
-            }
-        }
-        // Add System Message in memory
-        AgentState.updateState( state, Map.of( "memory", message ), AgentWorkflowState.SCHEMA );
-
-    }
-
-    @Override
-    public List<ChatMessage> messages() {
-        Objects.requireNonNull(state);
-        return state.memory();
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("memory clear is not implemented yet!");
+        super( "memory", AgentWorkflowState.SCHEMA );
     }
 }
